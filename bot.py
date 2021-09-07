26c32a04a377c04cc2e133ab22298ff6b0e1453e
SaitomTech <SaitomTech@fake.email>
2021-09-07 23:11:09 +0900

    [add] emoji reaction

--- bot.py
+++ bot.py
@@ -7,18 +7,30 @@ SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
 SLACK_APP_TOKEN = os.environ['SLACK_APP_TOKEN']
 CHANNEL_ID = os.environ['CHANNEL_ID']
 
-
 app = App(token=SLACK_BOT_TOKEN)
 
 
 @app.event('emoji_changed')
-def notify_emoji_added(event, say):
-    '''絵文字が追加されたときのみ通知する.'''
+def notify_emoji_added(event, say, client):
+    """絵文字が追加されたときのみ通知する."""
     if event['subtype'] == 'add':
         emoji_name = event['name']
 
-        say(text=f'{emoji_name}: :{emoji_name}:', channel=CHANNEL_ID)
-        say(text=f':{emoji_name}:', channel=CHANNEL_ID)
+        say(
+            text=f'新規スタンプ: `{emoji_name}`',
+            channel=CHANNEL_ID
+        )
+        message_emoji_info = say(
+            text=f':{emoji_name}:',
+            channel=CHANNEL_ID
+        )
+
+        client.reactions_add(
+            token=SLACK_BOT_TOKEN,
+            channel=CHANNEL_ID,
+            name=emoji_name,
+            timestamp=message_emoji_info['ts']
+        )
 
 
 if __name__ == '__main__':
